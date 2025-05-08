bde25b8157842dbadbf6ff3343907f7bdb59d91b
Guestding <memebigboy143@gmail.com>
2025-05-08 10:18:23 +1000

    made a draft level, added colliders and imported it into unity. Player moves in it too

---
+++ comp1151prac-tilemapgame-Guestding-main/Assets/Assets/Tiled/TinyDungeon_Tilesheet.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeon_Tilesheet" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <image source="../Kenny - Tiny Dungeon/Tilemap/tilemap.png" width="203" height="186"/>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="64">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="89">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="16" height="14"/>
+  </objectgroup>
+ </tile>
+</tileset>
