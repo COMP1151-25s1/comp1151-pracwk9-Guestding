01e28716779a301b6cb0fccecbd1d97936a23e04
Guestding <memebigboy143@gmail.com>
2025-05-22 10:23:25 +1000

    implemented physics

---
+++ comp1151prac-tilemapgame-Guestding-main/Assets/Assets/Tiled/physicsMap.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="physicsMap" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <image source="../Kenny - Tiny Dungeon/Tilemap/tilemap.png" width="203" height="186"/>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
